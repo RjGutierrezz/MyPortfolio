870d12e4d6a32bdc956d19465a76f21cc655b1bd
RjGutierrezz <rbg002@uark.edu>
2025-12-27 09:07:30 -0800

    added 3d model

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -2,6 +2,10 @@ import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { DirectionalLight, MeshStandardMaterial } from 'three'
 import {useMediaQuery} from 'react-responsive'
+import { Room } from './Room.jsx'
+import { Tokyo } from './Tokyo.jsx'
+import { Tokyo2 } from './Tokyo2.jsx'
+import { Test} from './Test.jsx'
 
 const HeroExperience = () => {
     const isTablet = useMediaQuery({query: '(max-width: 1024px)'});
@@ -9,7 +13,7 @@ const HeroExperience = () => {
   return (
     <Canvas camera={{position: [0, 0, 15], fov: 45}}>
         <ambientLight intensity ={0.2} color="#1a1a40"/>
-        <directionalLight position={[5, 5, 5]} intensity={1}/>
+        <directionalLight position={[5, 5, 5]} intensity={10}/>
 
         <OrbitControls
             enablePan={false}
@@ -20,10 +24,11 @@ const HeroExperience = () => {
             maxPolarAngle={Math.PI / 2}
         />
 
-        <mesh>
-            <boxGeometry args= {[1, 1, 1]} />
-            <meshStandardMaterial color="pink" />
-        </mesh>
+        {/* <Room /> */}
+        <group
+        scale={isMobile? 0.7 : 1} position={[0, 3.8, 0]} rotation={[0, -Math.PI / 40, 0.0]}>
+          <Tokyo2/>
+        </group>
     </Canvas>
   )
 }
